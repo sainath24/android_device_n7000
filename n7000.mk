e00ec98e51b0f7f1eb2201192437cba533f84d85
bauner <bauner@gmx.de>
2015-09-04 10:45:32 +0200

    Remove density from PRODUCT_AAPT_CONFIG

--- n7000.mk
+++ n7000.mk
@@ -23,7 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 $(call inherit-product, device/samsung/galaxys2-common/common.mk)
 
 # This device is xhdpi.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_LOCALES += xhdpi
 
