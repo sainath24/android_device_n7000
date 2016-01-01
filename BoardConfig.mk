43097e8dc9390c192fa3907d2124e6964a5bed02
sainath <sainathprasanna@gmail.com>
2016-01-01 20:25:21 +0530

    Change BoardConfig and dependencies to smdk4412

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,8 +25,8 @@ TARGET_BOARD_INFO_FILE := device/samsung/n7000/board-info.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n7000/bluetooth
 
 # Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/samsung/tw
-#TARGET_KERNEL_SOURCE := kernel/samsung/smdk4210
+#TARGET_KERNEL_SOURCE := kernel/samsung/tw
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_n7000_defconfig
 # assert
 TARGET_OTA_ASSERT_DEVICE := galaxynote,n7000,N7000,GT-N7000
